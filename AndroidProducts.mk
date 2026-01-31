e09245bf89bce92b5e4c55ea4287cafdde255e46
qteyrqer <qwe15328357161@163.com>
2026-01-31 18:48:29 +0800

    first commit

---
+++ AndroidProducts.mk
@@ -0,0 +1,14 @@
+#
+# Copyright (C) 2026 The Android Open Source Project
+# Copyright (C) 2026 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/omni_rk356x_box.mk
+
+COMMON_LUNCH_CHOICES := \
+    omni_rk356x_box-user \
+    omni_rk356x_box-userdebug \
+    omni_rk356x_box-eng
---
+++ omni_rk356x_box.mk
@@ -0,0 +1,29 @@
+#
+# Copyright (C) 2026 The Android Open Source Project
+# Copyright (C) 2026 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit some common TWRP stuff.
+$(call inherit-product, vendor/twrp/config/common.mk)
+
+# Inherit from rk356x_box device
+$(call inherit-product, device/rockchip/rk356x_box/device.mk)
+
+PRODUCT_DEVICE := rk356x_box
+PRODUCT_NAME := omni_rk356x_box
+PRODUCT_BRAND := Dangbei
+PRODUCT_MODEL := DBBOX5C
+PRODUCT_MANUFACTURER := rockchip
+
+PRODUCT_GMS_CLIENTID_BASE := android-rockchip-tv
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRIVATE_BUILD_DESC="rk356x_box-user 11 RD2A.211001.002 350 release-keys"
+
+BUILD_FINGERPRINT := Dangbei/rk356x_box/rk356x_box:11/RD2A.211001.002/350:user/release-keys
